fd9559939baf69edccde6be9ee6b46798d09e4d0
Hank Kapka <hankkapka@gmail.com>
2021-12-05 10:07:08 -0600

    both GET routes work, trouble shooting the POST route

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 const notes  = require('./db/db.json');
 // Helper method for generating unique ids
-const uuid = require('./assets/uuid');
+const uuid = require('./public/assets/uuid');
 
 const PORT = process.env.PORT || 3001;
 
@@ -24,13 +24,13 @@ function findById(id, notesArray) {
 
 //route to GET / notes and return the index.html file
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
   });
 //return the notes.html
 
 //route to GET / notes and return the notes.html file
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
   });
 
 
@@ -39,16 +39,44 @@ app.get('/api/notes', (req, res) => {
     res.json(notes);
   });
 
-// a route for a GET request that will return the content of our `db.json` file
+//use UUID to create a unique ID for each note
 
 //route to get note by id
   app.get('/api/notes/:id', (req, res) => {
     const result = findById(req.params.id, notes);
       res.json(result);
   });
-//POST /api/notes receives new note to save on the request body and add to the db.json file
 
-//use UUID to create a unique ID for each note
+//create a new note
+  //creates a new note
+function createNewNote(notes) {
+    newNotes = JSON.stringify(newNotes);
+    
+    fs.writeFileSync("./db/db.json", notes, function (err)
+    { if (err) {
+        return console.log(err);
+    }
+  });
+}
+//POST /api/notes receives new note to save on the request body and add to the db.json file
+app.post("api/notes",  (req, res) => {
+    if(notes.length == 0) {
+        req.body.id="0";
+    }else {
+    // set id based on what the next index of the array will be
+    req.body.id = JSON.stringify(JSON.parse(notes[notes.length - 1].id) + 1 );
+    }
+    notes.push(req.body);
+
+    //add note to notes
+    createNewNote(notes);
+    console.log(notes);
+
+    res.json(req.body);
+
+    });
+
+
 
 //BONUS to set up a DELETE api/notes/:id 
 
